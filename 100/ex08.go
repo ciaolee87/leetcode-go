088dbed51eccfb058da2d574d311c1ca634093c8
CiaoLee <ciaolee87@gmail.com>
2019-12-26 17:58:00 +0900

    Ex08 문제 풀이

--- 100/ex08.go
+++ 100/ex08.go
@@ -1,14 +1,28 @@
 package main
 
-import "strings"
+import (
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 func main() {
-
+	println(myAtoi("20000000000000000000"))
 }
 
 func myAtoi(str string) int {
 	str = strings.TrimSpace(str)
-	//str = strings
+	regex, _ := regexp.Compile("^[-+]?[0-9]+")
 
-	return 0
+	ext := regex.FindString(str)
+	num, _ := strconv.Atoi(ext)
+
+	if num < math.MinInt32 {
+		return math.MinInt32
+	} else if math.MaxInt32 < num {
+		return math.MaxInt32
+	} else {
+		return num
+	}
 }
